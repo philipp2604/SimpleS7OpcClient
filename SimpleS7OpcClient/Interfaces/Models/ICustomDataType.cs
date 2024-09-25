30ab9be0f73478216c97dca6bdda95e89476e7cc
Philipp Siewert <philipp@phsiewert.de>
2024-09-25 11:54:29 +0200

    Completed ICustomDataType interface

--- SimpleS7OpcClient/Interfaces/Models/ICustomDataType.cs
+++ SimpleS7OpcClient/Interfaces/Models/ICustomDataType.cs
@@ -1,6 +1,11 @@
-﻿namespace SimpleS7OpcClient.Interfaces.Models;
+﻿using SimpleS7OpcClient.Constants;
+
+namespace SimpleS7OpcClient.Interfaces.Models;
 
 public interface ICustomDataType
 {
     static abstract string TypeId { get; }
+    public Dictionary<string, (PlcDataType dataType, object? value)> Properties { get; }
+    public abstract object? Encode();
+    public abstract void Decode(object data);
 }
